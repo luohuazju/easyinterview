d5cc3c3a7441352b828d74397564c454593a0b9e
Hua Luo <hluo@lifesize.com>
2018-09-04 13:40:04 -0500

    remove duplicates from sorted array II

--- src/com/sillycat/easyinterview/leetcode/array/T80RemoveDuplicatesFromSortedArrayII.java
+++ src/com/sillycat/easyinterview/leetcode/array/T80RemoveDuplicatesFromSortedArrayII.java
@@ -1,5 +1,6 @@
 package com.sillycat.easyinterview.leetcode.array;
 
+import java.util.Arrays;
 
 /**
  * 
@@ -10,9 +11,40 @@ package com.sillycat.easyinterview.leetcode.array;
  */
 public class T80RemoveDuplicatesFromSortedArrayII {
 
-	public static void main(String[] args) {
-		
+	public static int removeDuplicates(int[] nums) {
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		if (nums.length == 1) {
+			return nums.length;
+		}
+		int j = 0; //slow pointer
+		int flag = 1; //flag to identify the times
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[j]) {
+				//not equal, move slow pointer to accept the value
+				j++;
+				flag = 1;
+			} else {
+				flag++; //equal, add up the flag
+				if(flag <= 2) { //not equal more than 2 times, move slow pointer to accepet value
+					j++;
+				}
+			}
+			nums[j] = nums[i];
+		}
+		return j + 1;
+	}
 
+	public static void main(String[] args) {
+		System.out.println("--------Remove Duplicates from Sorted Array II-----");
+		int[] sample1 = { 1, 1, 1, 2, 2, 3 };
+		System.out.println("New Length of " + Arrays.toString(sample1) + " is " + removeDuplicates(sample1));
+		System.out.println(Arrays.toString(sample1));
+		int[] sample2 = { 0, 0, 1, 1, 1, 1, 2, 3, 3 };
+		System.out.println("New Length of " + Arrays.toString(sample2) + " is " + removeDuplicates(sample2));
+		System.out.println(Arrays.toString(sample2));
+		System.out.println("---------------------------------------------------");
 	}
 
 }
